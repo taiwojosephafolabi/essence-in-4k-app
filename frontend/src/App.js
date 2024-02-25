77003585f0ef1d7079ccd6a852a6314108b1d023
JTAfolabi <taiwojosephafolabi@gmail.com>
2024-02-25 17:15:37 +0000

    landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LandingPage from './pages/landingPage.js';
+
 import './App.css';
 
-function App() {
+export default function App() {
+  const backToLandingPage = () => {
+    <Link to='/' />;
+  };
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          backToLandingPage={backToLandingPage}
+          exact
+          path='/'
+          element={<LandingPage />}
+        />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ frontend/src/pages/landingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import '../App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <h1>ESSENCE IN 4K</h1>
+    </div>
+  );
+}
+
+export default App;
